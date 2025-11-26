a6b51a973293eb307b4b56593891e95e60012c19
09bravo <157029034+09bravo@users.noreply.github.com>
2025-11-26 13:28:45 +0000

    Removed Waybar

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-su -c 'pacman -S swaybg sway foot waybar ttf-nerd-fonts-symbols ttf-fira-code dmenu'
+su -c 'pacman -S swaybg sway foot ttf-nerd-fonts-symbols ttf-fira-code dmenu'
 su -c 'sudo mv bin/* /bin/'
 rm -Rf wall-e/sources.txt
 mkdir -p ~/.config
-mv waybar/ sway/ foot/ ~/.config/
+mv sway/ foot/ ~/.config/
 mv wall-e ~/
